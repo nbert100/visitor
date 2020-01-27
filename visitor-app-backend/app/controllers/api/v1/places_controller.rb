1b629ba7d67b9c82e20d8b7649f1d58d27e1ff87
nbert100 <nbert14@gmail.com>
2020-01-26 21:11:05 -0500

    remove actions from places controller

--- visitor-app-backend/app/controllers/api/v1/places_controller.rb
+++ visitor-app-backend/app/controllers/api/v1/places_controller.rb
@@ -10,37 +10,37 @@ class Api::V1::PlacesController < ApplicationController
         render json: place, status: 200
     end
 
-    def create
-        place = Place.new(place_params)
-        if place.save
-            render json: place, status: 200
-         else 
-            render json: { errors: place.errors.full_messages }
-        end
-    end
+    # def create
+    #     place = Place.new(place_params)
+    #     if place.save
+    #         render json: place, status: 200
+    #      else 
+    #         render json: { errors: place.errors.full_messages }
+    #     end
+    # end
 
-    def update
-        set_place
-        if place.update(place_params)
-            render json: place
-            #flash message
-        else
-            render json: { errors: place.errors.full_messages }
-        end
-    end
+    # def update
+    #     set_place
+    #     if place.update(place_params)
+    #         render json: place
+    #         #flash message
+    #     else
+    #         render json: { errors: place.errors.full_messages }
+    #     end
+    # end
 
-    def destroy
-        set_place
-        place.destroy
-        render json: {placeId: place.id}
-    end
+    # def destroy
+    #     set_place
+    #     place.destroy
+    #     render json: {placeId: place.id}
+    # end
 
     private
     def set_place
         place = Place.find_by_id(params[:id])
     end
 
-    def place_params
-        params.require(:place).permit(:city, :country)
-    end
+    # def place_params
+    #     params.require(:place).permit(:city, :country)
+    # end
 end
