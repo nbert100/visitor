8dabd24acd9d456f3220be1d1e459937df333f46
nbert100 <nbert14@gmail.com>
2020-01-26 21:31:44 -0500

    correct visits controller

--- visitor-app-backend/app/controllers/api/v1/visits_controller.rb
+++ visitor-app-backend/app/controllers/api/v1/visits_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::VisitsController < ApplicationController
 
     def index
-        if params[:place_id]
-            place = Place.find_by_id(params[:place_id])
-            render json: place.visits
-        else
-            visits = Visit.all 
-            render json: visits, only: [:venue, :visitor]
-        end
+        # if params[:place_id]
+        #     place = Place.find_by_id(params[:place_id])
+        #     render json: place.visits
+        # else
+        #     visits = Visit.all 
+        #     render json: visits, only: [:venue, :visitor]
+        # end
+        visits = Visit.all 
+        render json: visits, status: 200
     end
 
     def show
@@ -16,29 +18,29 @@ class Api::V1::VisitsController < ApplicationController
     end
 
     def create
-        place = Place.find_or_create_by(city: place_params[:city])
+        # place = Place.find_or_create_by(city: place_params[:city])
         visit = place.visits.build(visit_params)
         if visit.save
-            render json: visit, include: :place, status: 201
-         else
-             render json: {errors: visit.errors.full_messages}
+            render json: visit, include: :place, status: 200
+        #  else
+        #      render json: {errors: visit.errors.full_messages}
         end
     end
 
-    def update
-        set_visit
-        if visit.update(visit_params)
-            render json: visit
-            #flash message
-        else
-            render json: { errors: visit.errors.full_messages }
-        end
-    end
+    # def update
+    #     set_visit
+    #     if visit.update(visit_params)
+    #         render json: visit
+    #         #flash message
+    #     else
+    #         render json: { errors: visit.errors.full_messages }
+    #     end
+    # end
 
     def destroy
         set_visit
         visit.destroy
-        render json: {visitId: visit.id}
+        render json: ("Trip successfully deleted!")
         #flash message
     end
 
@@ -48,9 +50,9 @@ class Api::V1::VisitsController < ApplicationController
         visit = Visit.find_by_id(params[:id])
     end
 
-    def place_params
-        params.require(:place).permit(:city, :country)
-    end
+    # def place_params
+    #     params.require(:place).permit(:city, :country)
+    # end
 
     def visit_params
         params.require(:visit).permit(:venue, :visitor, :when_visited, :visited, :comment, :place_id)
--- visitor-app-backend/app/models/visit.rb
+++ visitor-app-backend/app/models/visit.rb
@@ -1,5 +1,7 @@
 class Visit < ApplicationRecord
-  belongs_to :place
+  
   validates_presence_of :venue
   validates_presence_of :visitor
+
+  belongs_to :place
 end
